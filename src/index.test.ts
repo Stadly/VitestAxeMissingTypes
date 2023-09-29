2fc8c542f1c230a5ac36e89037769d6d4c89a2fc
Magnar Ovedal Myrtveit <magnar@norskhelseassistent.no>
2023-09-29 05:39:23 +0000

    Fix extending expect in test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { axe } from "vitest-axe";
+import "vitest-axe/extend-expect";
 
 it('has no violations', async () => {
 	expect(await axe("<main>test</main>")).toHaveNoViolations();
